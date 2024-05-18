f8faf75dd364f98d2fca03baa09a61504fc7c989
violetferret <kahlstro@ucsc.edu>
2024-05-18 14:37:00 -0700

    gettin started

--- src/Scenes/Level.js
+++ src/Scenes/Level.js
@@ -4,13 +4,13 @@ class Level extends Phaser.Scene {
         this.load.scenePlugin('AnimatedTiles', './lib/AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
 
         // Load character atlas
-        this.load.atlas("platformer_characters", "assets/kenney_pixel-platformer/Tilemap/tilemap-characters_packed.png", "assets/kenney_pixel-platformer/Tilemap/tilemap-characters-packed.json");
+        this.load.atlas("platformer_characters", "./assets/kenney_pixel-platformer/Tilemap/tilemap-characters_packed.png", "assets/kenney_pixel-platformer/Tilemap/tilemap-characters-packed.json");
 
         // Load tilemap information
-        this.load.image("base_tilemap_tiles", "assets/kenney_pixel-platformer/Tilemap/tilemap_packed.png");                         
-        this.load.image("farm_tilemap_tiles", "assets/kenney_pixel-platformer-farm-expansion/Tilemap/tilemap_packed.png");
-        this.load.image("backgrounds_tilemap_tiles", "assets/kenney_pixel-platformer/Tilemap/tilemap-backgrounds_packed.png");
-        this.load.tilemapTiledJSON("platformer-level-1", "assets/platformer-level-1.tmj");   // Tilemap in JSON
+        this.load.image("base_tilemap_tiles", "./assets/kenney_pixel-platformer/Tilemap/tilemap_packed.png");                         
+        this.load.image("farm_tilemap_tiles", "./assets/kenney_pixel-platformer-farm-expansion/Tilemap/tilemap_packed.png");
+        this.load.image("backgrounds_tilemap_tiles", "./assets/kenney_pixel-platformer/Tilemap/tilemap-backgrounds_packed.png");
+        this.load.tilemapTiledJSON("platformer-level-1", "./assets/platformer-level-1.tmj");   // Tilemap in JSON
     }
 
     constructor() {
@@ -19,7 +19,8 @@ class Level extends Phaser.Scene {
 
     create() {
         // Create new tilemap game object
-        this.map = this.add.tilemap("platformer-level-1", 18, 18, 245, 45);
+        // this.map = this.add.tilemap("platformer-level-1", 18, 18, 245, 45);
+        this.map = this.make.tilemap({key : "platformer-level-1"});
 
         // Load tileset
         this.base_tileset = this.map.addTilesetImage("base_tilemap", "base_tilemap_tiles");
@@ -27,19 +28,19 @@ class Level extends Phaser.Scene {
         this.backgrounds_tileset =  this.map.addTilesetImage("backgrounds_tilemap", "backgrounds_tilemap_tiles");
 
         // Layers
-        this.backgroundLayer =  this.map.createLayer("Background", this.bg_tileset, 0, 0);
+        this.backgroundLayer =  this.map.createLayer("Background", this.backgrounds_tileset, 0, 0);
         this.groundLayer = this.map.createLayer("Ground-Platforms", this.farm_tileset, 0, 0);
         this.plantsLayer = this.map.createLayer("Plants", this.farm_tileset, 0, 0);
         this.waterLayer = this.map.createLayer("Water", this.base_tileset, 0, 0);
         this.buildingsLayer = this.map.createLayer("Buildings", this.farm_tileset, 0, 0);
         this.collectiblesLayer = this.map.createLayer("Collectibles", this.base_tileset, 0, 0);
 
-        this.backgroundLayer.setScale(2);
-        this.groundLayer.setScale(2);
-        this.plantsLayer.setScale(2);
-        this.waterLayer.setScale(2);
-        this.buildingsLayer.setScale(2);
-        this.collectiblesLayer.setScale(2);
+        // this.backgroundLayer.setScale(2);
+        // this.groundLayer.setScale(2);
+        // this.plantsLayer.setScale(2);
+        // this.waterLayer.setScale(2);
+        // this.buildingsLayer.setScale(2);
+        // this.collectiblesLayer.setScale(2);
         
         // Make it collidable
         this.groundLayer.setCollisionByProperty({
